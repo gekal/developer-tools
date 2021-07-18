bc4aca3b4ff4fd72a54b0d1b79da9011fa0dc1de
gekal <liu.hongying@hotmail.com>
2021-07-18 16:49:56 +0900

    gradle

---
+++ gradle/setup.ps1
@@ -0,0 +1,20 @@
+$version = "7.1.1"
+
+Write-Host "Mavenファイルをダウンロードする。"
+Write-Host "https://services.gradle.org/distributions/gradle-${version}-bin.zip"
+$client = new-object System.Net.WebClient
+$client.DownloadFile("https://services.gradle.org/distributions/gradle-${version}-bin.zip", ".\gradle-${version}-bin.zip")
+
+Write-Host "ZIPファイルを解凍して、削除する。"
+Write-Host ".\gradle-${version}-bin.zip >>>> ."
+Expand-Archive -Path ".\gradle-${version}-bin.zip" -DestinationPath "." -Force
+Remove-Item ".\gradle-${version}-bin.zip"
+
+Write-Host "環境変数を設定する"
+$GRADLE_HOME = Resolve-Path ".\gradle-${version}"
+Write-Host "GRADLE_HOME = $GRADLE_HOME"
+[Environment]::SetEnvironmentVariable("GRADLE_HOME", $GRADLE_HOME, "User")
+Write-Host "Pathに[%GRADLE_HOME%\bin]を追加してください。"
+
+Write-Host "何かキーを押してください。" -NoNewLine
+[Console]::ReadKey() | Out-Null
