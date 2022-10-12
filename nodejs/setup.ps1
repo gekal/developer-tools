60443ce1cb63341a7270983c92ea24b62a0549d6
gekal <liu.hongying@hotmail.com>
2022-10-12 19:22:44 +0900

    nodejs

--- nodejs/setup.ps1
+++ nodejs/setup.ps1
@@ -1,10 +1,11 @@
 function init($version) {
-    Write-Host "Node.jsをダウンロードする。"
-    Write-Host "https://nodejs.org/dist/v${version}/node-v${version}-win-x64.zip"
-    $client = new-object System.Net.WebClient
-    $client.DownloadFile("https://nodejs.org/dist/v${version}/node-v${version}-win-x64.zip", ".\node-v${version}-win-x64.zip")
-
+    
     If (!(test-path ".\node-v${version}-win-x64")) {
+        Write-Host "Node.jsをダウンロードする。"
+        Write-Host "https://nodejs.org/dist/v${version}/node-v${version}-win-x64.zip"
+        $client = new-object System.Net.WebClient
+        $client.DownloadFile("https://nodejs.org/dist/v${version}/node-v${version}-win-x64.zip", ".\node-v${version}-win-x64.zip")
+
         Write-Host "ZIPファイルを解凍して、削除する。"
         Write-Host ".\node-v${version}-win-x64 >>>> ."
         Expand-Archive -Path ".\node-v${version}-win-x64.zip" -DestinationPath "." -Force
