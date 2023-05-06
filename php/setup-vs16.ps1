1f3fa511dae1fb2503da7e066e29e5de5a9aca44
gekal <liu.hongying@hotmail.com>
2023-05-06 16:41:27 +0900

    php: ts --> nts

--- php/setup-vs16.ps1
+++ php/setup-vs16.ps1
@@ -1,20 +1,20 @@
 function init($version) {
 
-    If (!(test-path ".\php-${version}-nts-Win32-vs16-x64")) {
+    If (!(test-path ".\php-${version}-Win32-vs16-x64")) {
         Write-Host "PHPをダウンロードする。"
-        Write-Host "https://windows.php.net/downloads/releases/php-${version}-nts-Win32-vs16-x64.zip"
+        Write-Host "https://windows.php.net/downloads/releases/php-${version}-Win32-vs16-x64.zip"
         $client = new-object System.Net.WebClient
         $client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-        $client.DownloadFile("https://windows.php.net/downloads/releases/php-${version}-nts-Win32-vs16-x64.zip", ".\php-${version}-nts-Win32-vs16-x64.zip")
+        $client.DownloadFile("https://windows.php.net/downloads/releases/php-${version}-Win32-vs16-x64.zip", ".\php-${version}-Win32-vs16-x64.zip")
 
         Write-Host "ZIPファイルを解凍して、削除する。"
-        Write-Host ".\php-${version}-nts-Win32-vs16-x64 >>>> ."
-        Expand-Archive -Path ".\php-${version}-nts-Win32-vs16-x64.zip" -DestinationPath ".\php-${version}-nts-Win32-vs16-x64" -Force
-        Remove-Item ".\php-${version}-nts-Win32-vs16-x64.zip"
+        Write-Host ".\php-${version}-Win32-vs16-x64 >>>> ."
+        Expand-Archive -Path ".\php-${version}-Win32-vs16-x64.zip" -DestinationPath ".\php-${version}-Win32-vs16-x64" -Force
+        Remove-Item ".\php-${version}-Win32-vs16-x64.zip"
     }
 
     Write-Host "環境変数を設定する"
-    $PHP_HOME = Resolve-Path ".\php-${version}-nts-Win32-vs16-x64"
+    $PHP_HOME = Resolve-Path ".\php-${version}-Win32-vs16-x64"
     Write-Host "PHP_HOME = $PHP_HOME"
     [Environment]::SetEnvironmentVariable("PHP_HOME", $PHP_HOME, "User")
     Write-Host "Pathに[%PHP_HOME%]を追加してください。"
